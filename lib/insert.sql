bf2fc6c369a99eb3d64e09243a71c6aac859053d
ruperthnyagesoa <ruperthnyagesoa@gmail.com>
2023-05-24 05:17:44 +0300

    Completed

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (id, name, age, sex, color, temperament) VALUES
+  (1, 'Mr. Chocolate', 5, 'male', 'brown', 'friendly'),
+  (2, 'Rowdy', 10, 'male', 'golden', 'calm'),
+  (3, 'Tabitha', 8, 'male', 'brown', 'hungry'),
+  (4, 'Sergeant Brown', 15, 'male', 'gray', 'jovial'),
+  (5, 'Melissa', 20, 'male', 'black', 'serious'),
+  (6, 'Grinch', 3, 'male', 'tan', 'curious'),
+  (7, 'Wendy', 1, 'female', 'white', 'playful'),
+  (8, NULL, 4, 'female', 'black', 'grumpy');
